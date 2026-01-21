0af7c77134ed4f735d5af64da96187922178f27a
aryanraj45 <rajaryan445567@gmail.com>
2026-01-21 17:09:41 +0530

    Refactor component: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// initial component update
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
