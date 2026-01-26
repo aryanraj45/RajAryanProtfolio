778b2002e3364ab4f38c02f660acb0d642fc335f
aryanraj45 <rajaryan445567@gmail.com>
2026-01-26 23:58:43 +0530

    refactor: add title prop support to NavLink

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -6,6 +6,7 @@ interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
   className?: string;
   activeClassName?: string;
   pendingClassName?: string;
+  title?: string;
 }
 //
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
