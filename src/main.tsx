72344b855dbdb87f3bf9b88d28da6352293b2ce5
aryanraj45 <rajaryan445567@gmail.com>
2026-01-26 23:59:14 +0530

    dx: wrap application in StrictMode for development

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-// initial component update
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
 
 // Maintenance update 14/15
